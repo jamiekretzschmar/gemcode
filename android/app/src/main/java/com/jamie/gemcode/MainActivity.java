1a2ebdeb1b6af8084da6f4d2f9441b5e88597594
jamiekretzschmar <jamie.kretzschamar@gmail.com>
2026-03-07 19:21:37 -0500

    Upload missing Capacitor files

---
+++ android/app/src/main/java/com/jamie/gemcode/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jamie.gemcode;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jamie.gemcode',
+  appName: 'com.jamie.gemcode',
+  webDir: 'dist'
+};
+
+export default config;
